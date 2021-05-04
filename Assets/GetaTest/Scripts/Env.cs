7805631fd9aa16bfec9e4a2748013715a6406d17
Cristian <crist.1707@hotmail.com>
2021-05-03 20:49:11 -0500

    Added game events and Env to have a const

---
+++ Assets/GetaTest/Scripts/Env.cs
@@ -0,0 +1,8 @@
+
+public static class Env 
+{
+    public const string GAME_SCENE = "TrackScene";
+    public const string MENU_SCENE = "MainMenuScene";
+    public const string LOADIING_SCENE = "LoadingScene";
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GetaGamesDevTest
 Unity game dev
+
+
+External
+
+- EventManager https://gist.github.com/bendangelo/093edb33c2e844c5c73a
